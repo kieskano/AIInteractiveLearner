a8d0b2ab55f23e67c8bca40362127ec6c6486af6
Han Hollander <han_hollander@hotmail.nl>
2017-01-14 17:25:45 +0100

    shadow (getE() and getChisq() done and done)

--- src/classifier/Test.java
+++ src/classifier/Test.java
@@ -1,41 +1,81 @@
 package classifier;
 
+import classifier.controller.FeatureSelect;
 import classifier.model.Word;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.SynchronousQueue;
 
 public class Test {
 
     public static void main(String[] args) {
-        Word word = new Word("Woord");
+        Word word1 = new Word("Woord nummero 1");
+        List<Double> list11 = new ArrayList<>();
+        List<Double> list12 = new ArrayList<>();
+        List<Double> list13 = new ArrayList<>();
+        list11.add(10.0);
+        list11.add(20.0);
+        list11.add(30.0);
+        list12.add(15.0);
+        list12.add(5.0);
+        list12.add(20.0);
+        list13.add(25.0);
+        list13.add(25.0);
+        list13.add(50.0);
+        Map<String, List<Double>> M1 = new HashMap<>();
+        M1.put("M", list11);
+        M1.put("V", list12);
+        M1.put("T", list13);
+        word1.setM(M1);
+        List<String> keys1 = new ArrayList<>();
+        keys1.add("M");
+        keys1.add("V");
+        keys1.add("T");
+        word1.setKeys(keys1);
+        System.out.println(word1);
 
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        List<Integer> list3 = new ArrayList<>();
-        list1.add(10);
-        list1.add(20);
-        list1.add(30);
-        list2.add(15);
-        list2.add(5);
-        list2.add(20);
-        list3.add(25);
-        list3.add(25);
-        list3.add(50);
-        Map<String, List<Integer>> M = new HashMap<>();
-        M.put("M", list1);
-        M.put("V", list2);
-        M.put("T", list3);
-        word.setM(M);
+        Word word2 = new Word("Woord nummero 2");
+        List<Double> list21 = new ArrayList<>();
+        List<Double> list22 = new ArrayList<>();
+        List<Double> list23 = new ArrayList<>();
+        List<Double> list24 = new ArrayList<>();
+        list21.add(10.0);
+        list21.add(20.0);
+        list21.add(30.0);
+        list22.add(15.0);
+        list22.add(5.0);
+        list22.add(20.0);
+        list23.add(20.0);
+        list23.add(15.0);
+        list23.add(35.0);
+        list24.add(45.0);
+        list24.add(40.0);
+        list24.add(85.0);
+        Map<String, List<Double>> M2 = new HashMap<>();
+        M2.put("M", list21);
+        M2.put("V", list22);
+        M2.put("N", list23);
+        M2.put("T", list24);
+        word2.setM(M2);
+        List<String> keys2 = new ArrayList<>();
+        keys2.add("M");
+        keys2.add("V");
+        keys2.add("N");
+        keys2.add("T");
+        word2.setKeys(keys2);
+        System.out.println(word2);
 
-        List<String> keys = new ArrayList<>();
-        keys.add("M");
-        keys.add("V");
-        keys.add("T");
-        word.setKeys(keys);
+        word1.setE(FeatureSelect.getE(word1));
+        System.out.println(word1);
 
-        System.out.println(word);
+        word2.setE(FeatureSelect.getE(word2));
+        System.out.println(word2);
+
+        System.out.println(FeatureSelect.getChisq(word1));
+        System.out.println(FeatureSelect.getChisq(word2));
+
     }
 }
--- src/classifier/controller/FeatureSelect.java
+++ src/classifier/controller/FeatureSelect.java
@@ -2,15 +2,77 @@ package classifier.controller;
 
 import classifier.model.Word;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FeatureSelect {
 
-    public static Word chisq(Word word) {
-        Word result = word;
-        int rows = 3;
-        int cols = result.getM().size();
+    public static Map<String, List<Double>> getE(Word word) {
+        Map<String, List<Double>> result = new HashMap<>();
+        Map<String, List<Double>> M = word.getM();
+        List<String> keys = word.getKeys();
+        double TW;
+        double TnW;
+        double N;
+        double TC;
+        double EW;
+        double EnW;
 
+        List<Double> T = M.get(keys.get(keys.size() - 1));
+        TW = T.get(0);
+        TnW = T.get(1);
+        N = T.get(2);
 
+        for (String key : keys) {
+            if (!key.equals(keys.get(keys.size() - 1))) {
+                TC = M.get(key).get(2);
+                EW = TC * TW / N;
+                EnW = TC * TnW / N;
+                List<Double> newE = new ArrayList<>();
+                newE.add(EW);
+                newE.add(EnW);
+                newE.add(TC);
+
+                result.put(key, newE);
+            }
+        }
+        result.put(keys.get(keys.size() - 1), M.get(keys.get(keys.size() - 1)));
         return result;
+    }
+
+    public static double getChisq(Word word) {
+        double result = 0;
+        Map<String, List<Double>> M = word.getM();
+        Map<String, List<Double>> E = word.getE();
+        List<String> keys = word.getKeys();
+        double Mij;
+        double Eij;
+        double chisq;
+
+        for (String key : keys) {
+            if (!key.equals(keys.get(keys.size() - 1))) {
+                for (int i = 0; i < 2; i++) {
+                    Mij = M.get(key).get(i);
+                    Eij = E.get(key).get(i);
+                    chisq = Math.pow(Mij - Eij, 2) / Eij;
+                    result = result + chisq;
+                }
+            }
+        }
+
+        round(result, 2);
+        return result;
+    }
+
+    public static double round(double value, int places) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
 }
