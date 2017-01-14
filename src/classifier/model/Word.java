9d99ee9ba4f82f145b882dc00021fd4e8acbd376
Han Hollander <han_hollander@hotmail.nl>
2017-01-14 15:01:39 +0100

    base word class

--- src/classifier/model/Word.java
+++ src/classifier/model/Word.java
@@ -1,7 +1,39 @@
 package classifier.model;
 
-/**
- * Created by han on 14-1-17.
- */
+import java.util.List;
+import java.util.Map;
+
 public class Word {
+
+    private String word;
+    private Map<String, List<Integer>> M;
+    private Map<String, List<Integer>> E;
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public Map<String, List<Integer>> getM() {
+        return M;
+    }
+
+    public void setM(Map<String, List<Integer>> m) {
+        M = m;
+    }
+
+    public Map<String, List<Integer>> getE() {
+        return E;
+    }
+
+    public void setE(Map<String, List<Integer>> e) {
+        E = e;
+    }
 }
