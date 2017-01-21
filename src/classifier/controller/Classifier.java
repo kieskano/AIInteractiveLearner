0842a79ccaca9cad80bc9878a0aca99c69a08e4b
Wijtse Rekker <wijtse@rekkerit.nl>
2017-01-21 15:53:09 +0100

    dinkie

--- src/classifier/controller/Classifier.java
+++ src/classifier/controller/Classifier.java
@@ -7,16 +7,16 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * Created by Wijtse on 17-1-2017.
+ * Created by Wijtse on 17-1-2017
  */
 public class Classifier {
 
-    private HashMap<String, Word> vocabulair;
+    private HashMap<String, Word> features;
 
-    public Classifier(List<Word> vocabulair) {
-        this.vocabulair = new HashMap<>();
-        for (Word word : vocabulair) {
-            this.vocabulair.put(word.getWord(), word);
+    public Classifier(List<Word> vocabulary) {
+        this.features = new HashMap<>();
+        for (Word word : vocabulary) {
+            this.features.put(word.getWord(), word);
         }
     }
 
