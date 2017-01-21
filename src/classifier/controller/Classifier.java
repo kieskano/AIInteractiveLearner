e247900d8ae2efe326c6802af805da4b453214a3
Wijtse Rekker <wijtse@rekkerit.nl>
2017-01-21 14:42:49 +0100

    Classifier start

---
+++ src/classifier/controller/Classifier.java
@@ -0,0 +1,22 @@
+package classifier.controller;
+
+import classifier.model.Document;
+
+/**
+ * Created by Wijtse on 17-1-2017.
+ */
+public class Classifier {
+
+
+    public Classifier() {
+
+    }
+
+    public String classify(Document document, VocabularyBuilder vocabulary) {
+        String result = "";
+
+
+        return result;
+    }
+
+}
--- src/classifier/controller/FeatureSelect.java
+++ src/classifier/controller/FeatureSelect.java
@@ -79,10 +79,6 @@ public class FeatureSelect {
         return result;
     }
 
-
-
-
-
     public static double round(double value, int places) {
         BigDecimal bd = new BigDecimal(value);
         bd = bd.setScale(places, RoundingMode.HALF_UP);
