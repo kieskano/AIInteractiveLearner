0019ee42efd4c57246146c7f7cd77550cc82c246
Han Hollander <han_hollander@hotmail.nl>
2017-01-21 15:02:55 +0100

    notin

---
+++ src/classifier/controller/Trainer.java
@@ -0,0 +1,18 @@
+package classifier.controller;
+
+public class Trainer {
+
+    /*
+    1. extract all words from the training set documents
+        variables: path
+    2. determine the best features for all classes
+        variables: amount of features per class
+    3.
+     */
+
+
+
+
+
+
+}
