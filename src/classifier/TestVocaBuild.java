c9a937dba3407a8a8ad39617e6753f318dcf4799
Wijtse Rekker <wijtse@rekkerit.nl>
2017-01-24 17:04:37 +0100

    Start TestVocaBuild

--- src/classifier/TestVocaBuild.java
+++ src/classifier/TestVocaBuild.java
@@ -9,10 +9,11 @@ public class TestVocaBuild {
 
 
     public static void main(String[] args) {
-        VocabularyBuilder vb = new VocabularyBuilder("testfiles");
+        VocabularyBuilder vb = new VocabularyBuilder("blogs");
         vb.loadWords();
         System.out.println("Loaded " + vb.getWordList().size() + " distinct words");
-        System.out.println(vb.getWordList());
+//        System.out.println(vb.getWordList());
+        vb.cleanVocabulary(5, 5000);
         System.out.println("Word list contains " + vb.getWordList().size() + " distinct words after cleaning");
 
     }
