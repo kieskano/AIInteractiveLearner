0bcbcdd5fa3d84a8e525d43475161a2a30db5dd3
Wijtse Rekker <wijtse@rekkerit.nl>
2017-01-25 11:43:59 +0100

    MegaTestdinkieoikjsejlkjasdflkjadfflkjads

--- src/classifier/MegaTest.java
+++ src/classifier/MegaTest.java
@@ -82,4 +82,8 @@ public class MegaTest {
 //    Classified 16 of 25 F-files correctly as F	(64.0%)
 //    Classified 10 of 25 M-files correctly as M	(40.0%)
 //    52.0% correctly classified
+
+
+//    [600, 6, 16051] 186%
+//    [1100, 1, 18551] 190%
 }
